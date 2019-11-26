c98b2baf6301ad2a746ce6bfb7a8336c4f22f250
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-11-25 21:48:52 -0500

    firs day of Scheduler

---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./DayListItem.scss"
+import classnames from "classnames"
+
+export default function DayListItem(props) {
+
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
+
+  const formatSpots = (spots) => {
+    if (spots === 1) {
+      return `${spots} spot remaining `
+    }
+    if (spots) {
+      return `${spots} spots remaining `
+    }
+    return "no spots remaining"
+  }
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
